1b65de248d8aa9f93945c02f4572c75858016fcd
Carlos Fiestas <cfiestas6@gmail.com>
2023-07-26 19:45:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bff26b41c5ed367dd2dc79afd03e36b0cceb81d9cee2d0ce25aeb43beea51d88
+size 28
---
+++ n-puzzle/npuzzle-gen.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab0426d531b6ffdf66dcb0bbb7ba01383676cdc38866ba5ac85955148d41e30c
+size 2275
